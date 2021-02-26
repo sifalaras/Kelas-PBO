1de1063a86af9ad5dd2976a322f28f60b21c8653
sifalaras <79291820+sifalaras@users.noreply.github.com>
2021-02-26 09:55:13 +0700

    Sifa Larasati (1902575)

---
+++ aktifitas3.py
@@ -0,0 +1,14 @@
+#PERANCANGAN
+print("percabangan pada bahasa pemrograman Python")
+print("pilih bahasa pemrograman: ")
+print("1. Phyton")
+print("2. C")
+
+pilih=int(input("pilih menu (1/2) : "))
+
+if pilih == 1:
+    print("bahasa pemrograman yang dipilih adalah Python")
+elif pilih == 2:
+    print("bahasa pemrograman yang dipilih adalah C")
+else :
+    print("pilihan Anda tidak tersedia!!")
