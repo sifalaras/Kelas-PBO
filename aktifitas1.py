2fbec056dfacd1ba3563b4b54a3923b5a4440a21
sifalaras <79291820+sifalaras@users.noreply.github.com>
2021-02-26 09:22:15 +0700

    Add files via upload

---
+++ aktifitas1.py
@@ -0,0 +1,11 @@
+#INPUT
+
+nama=input ("masukkan nama Anda: ")
+umur=19
+
+#OUTPUT
+print ("")
+print("====INPUT DAN OUTPUT PADA BAHASA PEMROGRAMAN PHYTON====")
+print("nama Anda: ", nama)
+print("umur Anda: ", umur)
+print("")
